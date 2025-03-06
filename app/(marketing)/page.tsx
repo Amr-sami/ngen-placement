bd1a09ec78ca08bc0403337faa0a9aac4ce41aaf
amrhe <amr.hassan.emam@gmail.com>
2025-03-06 13:10:12 +0200

    hot fix

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,11 @@
-import HomepageHero from "@/components/pages/Home/Hero";
-import HomepageAboutSection from "@/components/pages/Home/AboutSection";
-import HomepageRoadmapSection from "@/components/pages/Home/NgenRoadmapSection";
-import { TracksSection } from "@/components/general";
-import HomepageNgenForSection from "@/components/pages/Home/NgenForSection";
-import HomePageSocialProofSection from "@/components/pages/Home/SocialProofSection";
-import HomepageBannerSection from "@/components/pages/Home/BannerSection";
-import HomepageNgenWhySection from "@/components/pages/Home/NgenWhySection";
+import HomepageHero from '@/components/pages/Home/Hero';
+import HomepageAboutSection from '@/components/pages/Home/AboutSection';
+import HomepageRoadmapSection from '@/components/pages/Home/NgenRoadmapSection';
+import { TracksSection } from '@/components/general';
+import HomepageNgenForSection from '@/components/pages/Home/NgenForSection';
+// import HomePageSocialProofSection from "@/components/pages/Home/SocialProofSection";
+import HomepageBannerSection from '@/components/pages/Home/BannerSection';
+import HomepageNgenWhySection from '@/components/pages/Home/NgenWhySection';
 
 export default function Home() {
   return (
