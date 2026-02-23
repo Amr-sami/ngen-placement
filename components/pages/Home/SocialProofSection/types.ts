560e18208a4ce0a3e1599f79e3d62914c16f7a9c
Amr-sami <samyamr819@gmail.com>
2026-02-23 14:54:38 +0200

    fix: remove all orphaned Home/NgenFor/marketing components that reference deleted files

--- components/pages/Home/SocialProofSection/types.ts
+++
@@ -1,8 +0,0 @@
-export type SocialProofCardProps = {
-  variation: "images" | "testimonial";
-  image?: string;
-  fullName?: string;
-  avatarImg?: string;
-  feedbackMessage?: string;
-  feedbackRating?: number;
-};
