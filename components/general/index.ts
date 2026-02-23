0dfb2767a5a225fda259ab31a9bdbd281a6a5573
Amr-sami <samyamr819@gmail.com>
2026-02-23 14:45:57 +0200

    fix: remove TracksSection that references deleted data.json

--- components/general/index.ts
+++ components/general/index.ts
@@ -1,11 +1,9 @@
 import PageWrapper from './PageWrapper';
 import ContactUs from './ContactUs';
 import CallToAction from './CallToAction';
-import TracksSection from './TracksSection';
 
 export {
   PageWrapper,
   ContactUs,
   CallToAction,
-  TracksSection
 }
